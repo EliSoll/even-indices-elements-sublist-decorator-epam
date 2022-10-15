8c2b8ec7335f3758d39e5fc9a18a7a683c5db26a
Elvira Solnyshkina <elvira.solnyshkina@gmail.com>
2022-10-15 13:20:25 +0300

    v1: implement methods: - evenElementsQwerty;  evenElementsAbc;  evenElementsTen;  evenElementswithMod;

--- src/main/java/com/epam/rd/autocode/decorator/Decorators.java
+++ src/main/java/com/epam/rd/autocode/decorator/Decorators.java
@@ -1,9 +1,43 @@
 package com.epam.rd.autocode.decorator;
 
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class Decorators {
-    public static List<String> evenIndexElementsSubList(List<String> sourceList) {
-        throw new UnsupportedOperationException();
+public class Decorators extends AbstractList {
+    static List<String> myList;
+    static List<String> listTemp;
+    List<String> list;
+
+    public Decorators(List<String> sourceList) {
+        myList = sourceList;
     }
-}
+
+    public static List<String> evenIndexElementsSubList(List<String> sourceList) {
+
+        return new Decorators(sourceList);
+    }
+
+        public String get(int index){
+            listRecomposition();
+            return list.get(index);
+
+        }
+
+        public int size() {
+        listRecomposition();
+            return list.size();
+        }
+
+        public void listRecomposition() {
+            list = new ArrayList<>();
+            listTemp = myList;
+            for (int i = 0; i < listTemp.size(); i++){
+                if (i % 2 == 0){
+                    list.add(listTemp.get(i));
+                }
+            }
+        }
+    }
+
